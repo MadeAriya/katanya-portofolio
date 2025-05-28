ff4170c0e2838cee242b4c248df0c705c63f34cf
MadeAriya <madeariya28@gmail.com>
2025-05-28 15:34:34 +0800

    Cta misc

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -13,19 +13,19 @@ export default function Cta(){
                 <h1 className='font-montserrat font-bold text-[40px]'>Let's 🚀 Work Together</h1>
                 <p className='font-montserrat mt-5'>Need a developer to bring your idea to life? I’m here to help — let’s talk</p>
                 <div className='grid gap-3 mt-10 grid-cols-5'>
-                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#0077B5] rounded-[5] flex items-center justify-center'>
+                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#0077B5] rounded-[5px] flex items-center justify-center'>
                     <Image src="/images/linkedin.svg" alt="" width={30} height={30} className='my-auto'/>
                     </a>
-                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#E1306C] rounded-[5] flex items-center justify-center'>
+                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#E1306C] rounded-[5px] flex items-center justify-center'>
                     <Image src="/images/instagram.svg" alt="" width={30} height={30} className='my-auto'/>
                     </a>
-                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#000000] rounded-[5] flex items-center justify-center'>
+                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#000000] rounded-[5px] flex items-center justify-center'>
                     <Image src="/images/github.svg" alt="" width={60} height={60} className='my-auto'/>
                     </a>
-                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#5865F2] rounded-[5] flex items-center justify-center'>
+                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#5865F2] rounded-[5px] flex items-center justify-center'>
                     <Image src="/images/discord.svg" alt="" width={30} height={30} className='my-auto'/>
                     </a>
-                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#1877F2] rounded-[5] flex items-center justify-center'>
+                    <a href="https://www.linkedin.com/in/i-made-ariya-putra" className='w-[60px] h-[60px] bg-gradient-to-br from-[#D9D9D9] to-[#1877F2] rounded-[5px] flex items-center justify-center'>
                     <Image src="/images/facebook.svg" alt="" width={30} height={30} className='my-auto'/>
                     </a>
                     
