df70dbafdb4acd3ef44f3b8cd1593375a754692b
MadeAriya <madeariya28@gmail.com>
2025-06-29 18:53:14 +0800

    add: maintenance toggle

---
+++ src/app/[locale]/maintenance/page.jsx
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+      <h1>We're Under Maintenance</h1>
+      <p>Please check back later.</p>
+    </div>
+  )
+}
